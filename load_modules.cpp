0f0a9e67b7b0c62ceece70b474b6b883f5ce8cf7
ryan newman <74889691+irackit@users.noreply.github.com>
2021-07-28 19:13:35 -0400

    Update load_modules.cpp

--- load_modules.cpp
+++ load_modules.cpp
@@ -2,14 +2,27 @@
 #include "include/load_modules.h"
 
 ackit::load_modules::load_modules(){
-
+/*
     for(const auto& entry : std::filesystem::directory_iterator(PATH)){
         std::string path = entry.path().string();
         this->modules.push_back(path);
     }
-
     this->it = this->modules.begin();
-
+*/
+    DIR *dir;
+    dirent* ent;
+    if((dir = opendir(PATH)) != NULL){
+        while((ent = readdir(dir)) != NULL){
+            if(!strncmp(".",ent->d_name,MAX_LEN) || !strncmp("..",ent->d_name,MAX_LEN))continue;
+            std::string fullname = PATH;
+            fullname += ent->d_name;
+            this->modules.push_back(fullname);
+        }
+        closedir(dir);
+    }
+    else{
+        std::cout<<"file error"<<std::endl;
+    }
 }
     
 void ackit::load_modules::reset(void){
